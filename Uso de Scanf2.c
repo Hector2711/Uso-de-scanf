14c11661ee3fdbe06cab03bd3db4cc868588f090
Hector2711 <castrillorodriguezhector@gmail.com>
2024-08-31 11:36:39 -0600

    Add files via upload

---
+++ Uso de Scanf2.c
@@ -0,0 +1,20 @@
+//lIBRERIAS
+#include<stdio.h>
+
+//FUNCIÓN PRINCIPALl
+int main(){
+	
+	//VARIABLES LOCALES
+	float numero;
+	
+	//INSTRUCCIONES
+	//Reservamos memoria para un tipo de dato flotante.
+	printf("INgresa un número: ");
+	//Mandamos imprimir umensaae para que ingrese texto
+	scanf("%f",&numero);
+	//el scanf sirve para que el usuario pueda escribir
+	printf("%f",numero);
+	//este printf sirve para que el número  que el usuario escribio
+}
+
+
